d3a1150c9f61091d37ef2cfe8e0c52224f4ade05
Brett Bim <osiris43@gmail.com>
2011-06-01 11:40:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VendingMachine::Application.config.secret_token = '07a4ceadb4603054379a1291bfbf353978a8926a65fa9731003209b90bd6e87c0fd7dc7cf936f447637f454348107910369979fa6583cfc447befb855969222c'
